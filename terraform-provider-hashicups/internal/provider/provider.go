9e91ea7cc94137a861283825c8ea028407050e85
furon-kuina <tenma.edamura@gmail.com>
2024-10-01 00:50:01 +0900

    write provider type definition!

---
+++ terraform-provider-hashicups/internal/provider/provider.go
@@ -0,0 +1,47 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+var (
+	_ provider.Provider = &hashicupsProvider{}
+)
+
+func New(version string) func() provider.Provider {
+	return func() provider.Provider {
+		return &hashicupsProvider{
+			version: version,
+		}
+	}
+}
+
+type hashicupsProvider struct {
+	version string
+}
+
+func (p *hashicupsProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "hashicups"
+	resp.Version = p.version
+}
+
+func (p *hashicupsProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{}
+}
+
+func (p *hashicupsProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+
+}
+
+func (p *hashicupsProvider) DataSources(_ context.Context) []func() datasource.DataSource {
+	return nil
+}
+
+func (p *hashicupsProvider) Resources(_ context.Context) []func() resource.Resource {
+	return nil
+}
